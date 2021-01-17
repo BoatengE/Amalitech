52d621ea3e06d4405fd4c5575a22c4225a66fb31
BoatengE <elvis.boateng@amalitech.org>
2021-01-17 22:33:43 +0000

    gravity calculator

---
+++ src/Gravity.java
@@ -0,0 +1,22 @@
+
+  class GravityCalculator {
+        public static void main(String[] arguments) {
+            double gravity = -9.81; // Earth's gravity in m/s^2
+            double initialVelocity = 0.0;
+            double fallingTime = 10.0;
+            double initialPosition = 0.0;
+            double finalPosition = 0.0;
+//            System.out.println("The object's position after " + fallingTime +
+//                    " seconds is " + finalPosition + " m.");
+
+//            Unmodified value of the object's position in the unmodified code
+//            The object's position after 10.0 seconds is 0.0 m.
+
+            double accer = Math.pow((fallingTime),2);
+            finalPosition = ((gravity*accer)/2) + ((initialVelocity)*fallingTime) + initialPosition;
+            System.out.println("The object's position after " + fallingTime +
+                    " seconds is " + finalPosition + " m.");
+        }
+    }
+
+
